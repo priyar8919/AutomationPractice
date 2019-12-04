68e5d32c5e3e092ace3b8d9eb267b3d4a91eedf8
Priyadarshini.Ramasa <Priyadarshini.Ramasa@KANINI-LTP-180.gisbizdev.gisbiz.chennai.com>
2019-12-04 18:23:46 +0530

    Search shirt

---
+++ src/automationPractice/APSearch.java
@@ -0,0 +1,26 @@
+package automationPractice;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class APSearch {
+
+	public static void main(String[] args) {
+System.setProperty("webdriver.chrome.driver", "D:\\Desktopp\\Selenium\\libs\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("http://automationpractice.com/index.php");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.findElement(By.name("search_query")).sendKeys("Shirt");
+		driver.findElement(By.name("submit_search")).click();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		//driver.quit();
+
+	}
+
+}
