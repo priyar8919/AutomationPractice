ee88f471ba70330b4f7a70f9c21332f31628e58c
Priyadarshini.Ramasa <Priyadarshini.Ramasa@KANINI-LTP-180.gisbizdev.gisbiz.chennai.com>
2019-12-05 12:36:17 +0530

    link and partial link text

--- src/automationPractice/APSearch.java
+++ src/automationPractice/APSearch.java
@@ -15,10 +15,12 @@ System.setProperty("webdriver.chrome.driver", "D:\\Desktopp\\Selenium\\libs\\chr
 		
 		driver.get("http://automationpractice.com/index.php");
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-		driver.findElement(By.name("search_query")).sendKeys("Shirt");
-		driver.findElement(By.name("submit_search")).click();
-		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		//driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		//driver.findElement(By.name("search_query")).sendKeys("Shirt");
+		//driver.findElement(By.name("submit_search")).click();
+		driver .findElement(By.linkText("Sign in")).click();
+		driver.findElement(By.partialLinkText("Forgot yo")).click();
+		//driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		//driver.quit();
 
 	}
