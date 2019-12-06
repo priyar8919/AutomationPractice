1d6d6c905352e3addd1cbb48992bf2c7cdcb80d7
Priyadarshini.Ramasa <Priyadarshini.Ramasa@KANINI-LTP-180.gisbizdev.gisbiz.chennai.com>
2019-12-06 11:48:17 +0530

    Web element locators added

--- src/automationPractice/APSearch.java
+++ src/automationPractice/APSearch.java
@@ -15,11 +15,13 @@ System.setProperty("webdriver.chrome.driver", "D:\\Desktopp\\Selenium\\libs\\chr
 		
 		driver.get("http://automationpractice.com/index.php");
 		driver.manage().window().maximize();
-		//driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-		//driver.findElement(By.name("search_query")).sendKeys("Shirt");
+		//driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);            //implicit wait
+		//driver.findElement(By.name("search_query")).sendKeys("Shirt");            //by name
 		//driver.findElement(By.name("submit_search")).click();
-		driver .findElement(By.linkText("Sign in")).click();
-		driver.findElement(By.partialLinkText("Forgot yo")).click();
+		driver .findElement(By.linkText("Sign in")).click();                        //by link text
+		driver.findElement(By.partialLinkText("Forgot yo")).click();                //by partial link text
+		driver.findElement(By.cssSelector(".login")).click();                       // by css selector
+		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("testmail@demo.com");
 		//driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		//driver.quit();
 
