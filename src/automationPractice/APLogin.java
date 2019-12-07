c9ccb8aacc1dbc3b15797396d89f37b91a0cdfad
DELL <DELL@DESKTOP-UCHTIHI>
2019-12-07 22:19:26 +0530

    new machine changes

--- src/automationPractice/APLogin.java
+++ src/automationPractice/APLogin.java
@@ -11,16 +11,16 @@ public class APLogin {
 	public static void main(String[] args) {
 		// http://chromedriver.storage.googleapis.com/index.html
 		//String baseURL = "http://automationpractice.com/index.php";
-		System.setProperty("webdriver.chrome.driver", "D:\\Desktopp\\Selenium\\libs\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "D:\\Selenium\\libs\\chromedriver.exe");
 		
 		WebDriver driver = new ChromeDriver();
 		
 		driver.get("http://automationpractice.com/index.php");
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		//driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		driver.findElement(By.name("search_query")).sendKeys("Shirt");
-		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-		driver.quit();
+		//driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		//driver.quit();
 		
 
 	}
