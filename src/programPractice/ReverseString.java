e597a75cc92db87fcd6253b4c5d0507b6778ad07
Priyadarshini.Ramasa <Priyadarshini.Ramasa@KANINI-LTP-180.gisbizdev.gisbiz.chennai.com>
2020-01-06 16:45:56 +0530

    small change

--- src/programPractice/ReverseString.java
+++ src/programPractice/ReverseString.java
@@ -6,8 +6,10 @@ public class ReverseString {
 	public static void main(String[] args) {
 		String input = "Hello World";
 		char[] try1 = input.toCharArray();
-		for(int i= try1.length-1; i>=0; i--) 
+		for(int i= try1.length-1; i>=0; i--) {
+			
+		
 		System.out.print(try1[i]);    // Use 'print' instead of 'println' to print in single line
 	}
-
+	}
 }
