51198a32abebebc375c64f05e586519399d704bc
Priyadarshini.Ramasa <Priyadarshini.Ramasa@KANINI-LTP-180.gisbizdev.gisbiz.chennai.com>
2020-02-05 12:15:29 +0530

    floyds

---
+++ src/programPractice/FloydsTriangle.java
@@ -0,0 +1,27 @@
+package programPractice;
+
+import java.util.Scanner;
+
+public class FloydsTriangle {
+	
+	public static void main(String[] args) {
+		int i, j, num=1;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter number of rows");
+		int RowNum = sc.nextInt();
+		
+		System.out.println("Floyd's Triangle is:");
+		for (i=1; i<=RowNum; i++) {
+			for(j=1; j<=i; j++) {
+				
+				System.out.print(num +" ");
+				num++;
+			}
+			System.out.println();
+		}
+		
+		
+	}
+
+}
